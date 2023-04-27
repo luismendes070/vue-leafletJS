3afedc88965f56d245b8b9eb0c89ec3659158977
luismendes070 <machadoluismendes@gmail.com>
2023-04-27 12:39:18 -0300

    1 file modified

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,3 +1,4 @@
+<script>
 mounted() {
   // initialize the map
   const map = L.map("map").setView([51.505, -0.09], 13);
@@ -47,3 +48,11 @@ mounted() {
 
   L.geoJSON(geojson).addTo(map);
 }
+
+export default {
+  name: 'Map',
+  props: {
+  }
+}
+
+</script>
